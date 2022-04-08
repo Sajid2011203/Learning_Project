330bab519c11fec5ae1cd439183c236477480327
MD Badimma <mdbadimma24434@gmail.com>
2022-04-09 05:29:03 +0600

    I have create show hide function

--- JavaScript/Date.js
+++ JavaScript/Date.js
@@ -2,21 +2,23 @@
 //1
 var a = new Date();
 
-console.log(a)
-console.log(a.toDateString())
-console.log(a.toLocaleDateString())
-console.log(a.toTimeString())
+// console.log(a)
+// console.log(a.toDateString())
+// console.log(a.toLocaleDateString())
+// console.log(a.toTimeString())
 
-console.log(a.getDate())
-console.log(a.getMonth())
-console.log(a.getFullYear())
+// console.log(a.getDate())
+// console.log(a.getMonth())
+// console.log(a.getFullYear())
 
-console.log(a.getHours())
-console.log(a.getMinutes())
-console.log(a.getSeconds())
-console.log(a.getMilliseconds())
+// console.log(a.getHours())
+// console.log(a.getMinutes())
+// console.log(a.getSeconds())
+// console.log(a.getMilliseconds())
 
-var b = new Date()
-console.log(a.setDate())
-console.log(b.setFullYear(2020))
-console.log(a.setHours())
+// var b = new Date()
+// console.log(a.setDate())
+// console.log(b.setFullYear(2020))
+// console.log(a.setHours())
+
+console.log(new Date())
