1a76639e6cf12ea73161250ae6e67ade6fe21b11
MD Badimma <mdbadimma24434@gmail.com>
2022-04-17 11:42:16 +0600

    sobe topic update & switch topic is new

---
+++ JavaScript/switch.js
@@ -0,0 +1,39 @@
+var number = 45
+
+switch(number === ) {
+    case number < 45:
+        console.log('') 
+}
+
+
+
+
+// Get Day
+
+let today = new Date().getDay()
+
+switch(today) {
+    case 0:
+        console.log('Today Is Sunday')
+        break
+    case 1:    
+        console.log('Today Is Monday')
+        case 2:
+        console.log('Today Is Tuesday')
+        break
+    case 3:    
+        console.log('Today Is Wednesday')
+        break
+        case 4:
+        console.log('Today Is Thursday')
+        break
+    case 5:    
+        console.log('Tday is Friday')
+        break
+    case 6: 
+    console.log('Today Is saturday')
+    break
+    
+    default:
+    console.log('invalid day')
+}
